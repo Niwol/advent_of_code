6f483a991be758f5c0a3ca423494475af911477d
Niwol <lowin000@gmail.com>
2021-12-04 11:30:43 +0100

    Finished day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -9,6 +9,7 @@ enum Direction {
 fn main() {
     let data = fs::read_to_string("data.txt").unwrap();
 
+    // V1
     let mut x = 0;
     let mut y = 0;
 
@@ -34,4 +35,34 @@ fn main() {
         });
 
     println!("V1: {}", x * y);
+
+    // V2
+    x = 0;
+    y = 0;
+    let mut aim = 0;
+
+    data.split('\n')
+        .map(|s| {
+            let command = s.split(' ').next().unwrap();
+            let val = s.split(' ').skip(1).next().unwrap().parse::<i32>().unwrap();
+
+            match command {
+                "forward" => Forward(val),
+                "up" => Up(val),
+                "down" => Down(val),
+                _ => panic!("Wrog command"),
+            }
+        })
+        .for_each(|command| {
+            match command {
+                Forward(v) => {
+                    x += v;
+                    y += v * aim;
+                }
+                Up(v) => aim -= v,
+                Down(v) => aim += v,
+            };
+        });
+
+    println!("V2: {}", x * y);
 }
